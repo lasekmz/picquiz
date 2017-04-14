21e46259906c7476764f68dbb7aa198d3ac6b24e
Zach <lasekmz@dukes.jmu.edu>
2017-04-14 09:48:41 -0400

    added first test and attempt at making it pass

---
+++ js/index.js
@@ -0,0 +1,9 @@
+require('./jquery.csv.js');
+var $ = jQuery = require('./jquery.csv.js');
+
+//var columns = ["column1", "column2", "column3"];
+//("csv-to-array")({
+  // file: "C:/Users/Zach/Desktop/Programming/Projects/picquiz/js",
+  // function(err, array)};
+
+module.exports.takein = data => $.csv.toArrays(csv, {}, function(err, data);
